2fcedfde6b4a2e83aa911a2b1d2f4af1f25921e1
medresj <leo.s@fnet.cc>
2026-07-26 11:24:41 +0200

    it actually does something now

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 
-const g = 9.81
+const const1 = 1.167 * 10 ** -8;
+const const2 = 1.408 * 10 ** -7;
+
+function energyfunc() {
+  const value = document.getElementById('energy').value;
+  document.getElementById("demo").innerHTML = "Energy is " + value + " Joules";
+}
+
+function handleToggle() {
+    const isOn = document.getElementById('toggle').checked;
+    document.getElementById('status').textContent = `straight up?: ${isOn}`;
+    console.log(isOn);
+}
+
+ const textBox = document.getElementById('textBox');
+  const output = document.getElementById('output');
+
+  textBox.addEventListener('input', () => {
+    output.textContent = `Energy: ${textBox.value}`;
+  });
 
 function calculate() {
-    document.getElementById("demo").innerHTML = "nothing to calculate.";
+    if (document.getElementById('toggle').checked) {
+        const payload = const2 * energyinput.value;
+        document.getElementById('result').textContent = `Result: ${payload} kg`;
+    
+    } else {
+        const payload = const1 * energyinput.value;
+        document.getElementById('result').textContent = `Result: ${payload} kg`;
+    }
 }
